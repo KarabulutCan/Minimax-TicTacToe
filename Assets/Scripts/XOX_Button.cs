4cbe34c40fce7f97e349b3637de27ade245fc822
cankarabulut <rysesoft@gmail.com>
2025-02-15 01:04:24 +0300

    first commit

---
+++ Assets/Scripts/XOX_Button.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class XOX_Button : MonoBehaviour
+{
+    public int id;
+    private Button button;
+    private Image image;
+
+    private void Awake()
+    {
+        image = GetComponent<Image>();
+        button = GetComponent<Button>();
+    }
+
+    public void ReportClick(bool isPlayerClick = true)
+    {
+        if(!GameManager.IsInputAllowed() && isPlayerClick)
+        {
+            return;
+        }
+
+        MarkButtonAsPressed();
+        GameManager.MakeMove(id);
+    }
+
+    public void MarkButtonAsPressed()
+    {
+        image.sprite = GameManager.GetSideSprite();
+        button.interactable = false;
+    }
+}
